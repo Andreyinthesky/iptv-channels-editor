f142aef7f4e3868a914ed5e6fab1db3e21c920c7
Andreyinthesky <and19980316@gmail.com>
2019-06-07 00:35:44 +0500

    cleanup unused code

--- cs/IptvChannelsEditor.Web/ClientApp/src/components/ChannelTableRow.js
+++ cs/IptvChannelsEditor.Web/ClientApp/src/components/ChannelTableRow.js
@@ -41,12 +41,6 @@ const channelTableRowStyles = theme => ({
 class ChannelTableRow extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      isEdit : false,
-    };
-    this.titleRef = React.createRef();
-    this.groupTitleRef = React.createRef();
-    this.pathRef = React.createRef();
   }
 
   handleSelectChannel = () => {
@@ -58,16 +52,6 @@ class ChannelTableRow extends Component {
   handleEditChannel = () => {
     this.props.onEditChannel(this.props.index);
   };
-
-  handleCancelEditChannel = () => {
-    let channel = this.props.channel;
-    console.log('cancel');
-    this.titleRef.current.value = channel.title;
-    this.groupTitleRef.current.value = channel.groupTitle;
-    this.pathRef.current.value = channel.path;
-    this.setState({isEdit: !this.state.isEdit});
-  };
-  
   
   handleInsertBeforeChannel = () => {
     this.props.onInsertChannel(this.props.index);
@@ -118,17 +102,9 @@ class ChannelTableRow extends Component {
           </IconButton>
           <Tooltip title={'Редактировать'}>
             <IconButton onClick={this.handleEditChannel} aria-label="Edit">
-              {this.state.isEdit ? <DoneIcon /> : <EditIcon />}
+              <EditIcon />
             </IconButton>
           </Tooltip>
-          {
-            this.state.isEdit &&
-            <Tooltip title='Cancel'>
-              <IconButton onClick={this.handleCancelEditChannel} aria-label="Cancel">
-                <CancelIcon />
-              </IconButton>
-            </Tooltip>
-          }
           <InsertChannelMenu
             onInsertBeforeChannel={this.handleInsertBeforeChannel}
             onInsertAfterChannel={this.handleInsertAfterChannel}
